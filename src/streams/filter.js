bd465dc89340d6cb0244862a0f1541d1bd92565e
marygrishchuk <mary.grishchuk1@gmail.com>
2026-03-09 02:21:58 +0300

    feat: implement filter.js

--- src/streams/filter.js
+++ src/streams/filter.js
@@ -1,9 +1,37 @@
+import { Transform, pipeline } from 'stream';
+
 const filter = () => {
   // Write your code here
   // Read from process.stdin
   // Filter lines by --pattern CLI argument
   // Use Transform Stream
   // Write to process.stdout
+  const patternIndex = process.argv.indexOf('--pattern');
+  const pattern = patternIndex >= 0 ? process.argv[patternIndex + 1] : null;
+  const patternRegex = new RegExp(pattern);
+
+  const transformer = new Transform({
+    transform(chunk, _encoding, callback) {
+      // splitting the chunk into lines if it contains \n:
+      const lines = chunk.toString().includes('\\n') ? chunk.toString().split('\\n') : [chunk.toString()];
+
+      // iterating over all lines:
+      lines.forEach(line => {
+        if (patternRegex.test(line) || !pattern) {
+          this.push(line + '\n');
+        }
+      });
+
+      callback();
+    }
+  });
+
+  // connecting stdin -> transformer -> stdout:
+  pipeline(process.stdin, transformer, process.stdout, (err) => {
+    if (err) {
+      console.error('Pipeline failed:', err);
+    }
+  });
 };
 
 filter();
